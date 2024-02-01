b243e7a8179d07607cedab7d02fbb2933bd5ec1e
loiphan <loipnb.vbee@gmail.com>
2024-02-01 10:50:54 +0700

    Add scss style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import { TimeArea } from "./components/timeArea";
 
 const App = () => {
--- src/components/timeArea/index.tsx
+++ src/components/timeArea/index.tsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { Grid, GridCellRenderer } from "react-virtualized";
+import "./style.scss";
 
 export const TimeArea = () => {
   const cellRenderer: GridCellRenderer = ({ columnIndex, key, style }) => {
+    const classNames = ["time-unit"];
+    if (columnIndex) classNames.push("time-unit-big");
     return (
-      <div key={key} style={style}>
-        {columnIndex}
+      <div key={key} style={style} className={classNames.join(" ")}>
+        <div className="time-unit-scale">{columnIndex}</div>
       </div>
     );
   };
 
   return (
-    <Grid
-      columnCount={10}
-      columnWidth={500}
-      rowCount={1}
-      rowHeight={50}
-      width={1000}
-      height={50}
-      overscanRowCount={0} // tranhs truowng hop ko load kip khi scroll
-      overscanColumnCount={10} // giam thieu truowng hop khong load kip cac item khi keo sang trai
-      cellRenderer={cellRenderer}
-    />
+    <div className="time-area">
+      <Grid
+        columnCount={10}
+        columnWidth={500}
+        rowCount={1}
+        rowHeight={50}
+        width={1000}
+        height={50}
+        overscanRowCount={0} // tranhs truowng hop ko load kip khi scroll
+        overscanColumnCount={10} // giam thieu truowng hop khong load kip cac item khi keo sang trai
+        cellRenderer={cellRenderer}
+      />
+    </div>
   );
 };
